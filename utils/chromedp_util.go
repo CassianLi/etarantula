b8f67856e89f629af1ca1427899c1b2c9c2fbe70
jinrunli <ljr@y-clouds.com>
2024-03-07 15:36:59 +0800

    fix: ebay selector无法找到时没有超时结束

--- utils/chromedp_util.go
+++ utils/chromedp_util.go
@@ -80,8 +80,7 @@ func Navigate(ctx context.Context, url string) error {
 // NavigateAndWait 打开URL,等待selector元素加载完成
 func NavigateAndWait(ctx context.Context, url string, selector string, timeout time.Duration) error {
 	// 创建带超时的上下文
-	tctx, tcancel := context.WithTimeout(ctx, timeout)
-	defer tcancel()
+	tctx, _ := context.WithTimeout(ctx, timeout)
 	// navigate to the URL
 	return chromedp.Run(tctx, chromedp.Navigate(url), chromedp.WaitVisible(selector))
 }
